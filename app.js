d690838e951e726365e64ef6dc678bf469f47e92
José Andrés <joseacevedofallas@gmail.com>
2025-08-30 21:12:05 -0600

    Agregar Amigo

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    console.log(amigo);
+
+    if (esTextoValido(amigo)){
+        amigos.push(amigo);
+    console.log(amigos);
+    } else {
+        alert("Solo se permiten letras y espacios.");
+    }
+    limpiarCaja();
+}
+
+function esTextoValido(amigo) {
+  if (typeof amigo !== "string") return false; 
+  if (!amigo.trim()) return false;
+
+  const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+  return regex.test(amigo);
+}  
+
+function limpiarCaja(){
+   document.querySelector('#amigo'). value = '';
+}
